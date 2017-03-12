d030bcb9b6edf5eaa4f628f7280a9ba1b9209dc6
Rado <bsawsk@hotmail.com>
2017-03-12 23:01:11 +0200

    changed from in-memory storage to db.

--- app/src/main/java/com/abbytech/shoppingapp/repo/InMemoryItemRepo.java
+++ app/src/main/java/com/abbytech/shoppingapp/repo/InMemoryItemRepo.java
@@ -1,7 +1,9 @@
 package com.abbytech.shoppingapp.repo;
 
 
+import com.abbytech.shoppingapp.ShoppingApp;
 import com.abbytech.shoppingapp.model.Item;
+import com.abbytech.shoppingapp.model.ItemDao;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,16 +11,30 @@ import java.util.List;
 import rx.Observable;
 
 public class InMemoryItemRepo implements ItemRepo {
-    List<Item> items = new ArrayList<>();
+    private static ItemRepo instance;
+
+    private final ItemDao itemDao;
 
     public InMemoryItemRepo() {
-        items.add(new Item("Cheese","Dairy"));
-        items.add(new Item("Milk","Dairy"));
-        items.add(new Item("Yogurt","Dairy"));
+        itemDao = ShoppingApp.getInstance().getDao().newSession().getItemDao();
+        if (itemDao.load(1L)==null) {
+            List<Item> items = new ArrayList<>();
+            items.add(new Item("Cheese", "Dairy"));
+            items.add(new Item("Milk", "Dairy"));
+            items.add(new Item("Yogurt", "Dairy"));
+            for (Item item : items) {
+                itemDao.save(item);
+            }
+        }
     }
-
+    public static ItemRepo getInstance(){
+        if (instance==null)
+            instance = new InMemoryItemRepo();
+        return instance;
+    }
     @Override
     public Observable<List<Item>> getAllItems() {
+        List<Item> items = itemDao.loadAll();
         return Observable.just(items);
     }
 }
