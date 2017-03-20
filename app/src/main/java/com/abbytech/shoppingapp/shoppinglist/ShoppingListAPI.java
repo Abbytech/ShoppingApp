44fd92af4ba7ce0d211e183720aeb67c890650d1
Rado <bsawsk@hotmail.com>
2017-03-20 23:06:18 +0200

    .

--- app/src/main/java/com/abbytech/shoppingapp/shoppinglist/ShoppingListAPI.java
+++ app/src/main/java/com/abbytech/shoppingapp/shoppinglist/ShoppingListAPI.java
@@ -6,17 +6,19 @@ import com.abbytech.shoppingapp.model.ShoppingList;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.adapter.rxjava.Result;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import rx.Observable;
 
+// TODO: 20/03/2017 implement this
 public interface ShoppingListAPI {
     @GET("/{id}")
     Observable<ShoppingList> getShoppingList(@Path("id") int id);
     @POST("/")
-    Observable<Result> saveShoppingItem(ListItem item);
+    Observable<Result<ResponseBody>> saveShoppingItem(ListItem item);
     @GET("/")
     Observable<List<ShoppingList>> getShoppingLists();
 }
