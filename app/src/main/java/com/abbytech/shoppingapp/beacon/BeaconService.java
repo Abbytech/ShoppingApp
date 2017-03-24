253177aab56b6429a7b1fd2bd96bc10777734858
Rado <bsawsk@hotmail.com>
2017-03-24 13:04:09 +0200

    beaconOptions are now loaded from resources.

--- app/src/main/java/com/abbytech/shoppingapp/beacon/BeaconService.java
+++ app/src/main/java/com/abbytech/shoppingapp/beacon/BeaconService.java
@@ -13,13 +13,20 @@ import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.logging.LogManager;
 
-public class BeaconService extends Service implements BeaconConsumer{
+public class BeaconService extends Service implements BeaconConsumer {
     private static final String TAG = "test";
-    private final Region diaryRegion = RegionFactory.createRegion("Dairy","0x00000000");
-    private final Region bakeryRegion = RegionFactory.createRegion("Bakery","0x11111111");
+    private final Region diaryRegion = RegionFactory.createRegion("Dairy", "0x00000000");
+    private final Region bakeryRegion = RegionFactory.createRegion("Bakery", "0x11111111");
     private BeaconManager beaconManager;
     private LocalBinder<BeaconService> binder = new LocalBinder<>(this);
+
     public BeaconService() {
+    }
+
+    public static void setupBeaconManager(BeaconManager beaconManager, BeaconServiceOptions options) {
+        BeaconManager.setRegionExitPeriod(options.getExitPeriod());
+        BeaconParser parser = new BeaconParser().setBeaconLayout(options.getBeaconLayout());
+        beaconManager.getBeaconParsers().add(parser);
     }
 
     public BeaconManager getBeaconManager() {
@@ -33,19 +40,16 @@ public class BeaconService extends Service implements BeaconConsumer{
         beaconManager.bind(this);
     }
 
-    public static void setupBeaconManager(BeaconManager beaconManager){
-        BeaconManager.setRegionExitPeriod(1000);
-        BeaconParser parser = new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-11,i:12-15,i:16-19,p:24-24");
-        beaconManager.getBeaconParsers().add(parser);
-    }
     @Override
     public void onDestroy() {
         beaconManager.unbind(this);
     }
+
     @Override
     public void onBeaconServiceConnect() {
         Log.d(TAG, "onBeaconServiceConnect: connected");
-        setupBeaconManager(beaconManager);
+        BeaconServiceOptions beaconServiceOptions = BeaconServiceOptions.fromResources(this);
+        setupBeaconManager(beaconManager, beaconServiceOptions);
         try {
             beaconManager.startMonitoringBeaconsInRegion(diaryRegion);
             beaconManager.startMonitoringBeaconsInRegion(bakeryRegion);
@@ -67,6 +71,7 @@ public class BeaconService extends Service implements BeaconConsumer{
 
     public static class LocalBinder<T extends Service> extends Binder {
         T service;
+
         public LocalBinder(T service) {
             this.service = service;
         }
