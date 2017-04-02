f67d76f8cf412eacdc99e52d14ab0b9fcc11ad74
Rado <bsawsk@hotmail.com>
2017-04-02 19:43:13 +0300

    fixed incorrect annotation.

--- app/src/main/java/com/abbytech/shoppingapp/shop/ShopAPI.java
+++ app/src/main/java/com/abbytech/shoppingapp/shop/ShopAPI.java
@@ -5,17 +5,17 @@ import com.abbytech.shoppingapp.model.Item;
 
 import java.util.List;
 
-import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 import rx.Observable;
 
 public interface ShopAPI {
     @GET("Items/All/")
     Observable<List<Item>> getItems();
 
-    @GET("Items/All?section={section}")
-    Observable<List<Item>> getItems(@Field("section") String section);
+    @GET("Items/All")
+    Observable<List<Item>> getItems(@Query("section") String section);
     @GET("Items/Get/{id}")
     Observable<Item> getItem(@Path("id") int id);
 }
