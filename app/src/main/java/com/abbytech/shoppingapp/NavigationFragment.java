d3a1216ce43baae51f98a9ea498acdc99fd955d8
Rado <bsawsk@hotmail.com>
2017-04-02 21:37:25 +0300

    fixed bug where fragment back-stack is maintained despite navigating to a different fragment.

--- app/src/main/java/com/abbytech/shoppingapp/NavigationFragment.java
+++ app/src/main/java/com/abbytech/shoppingapp/NavigationFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -32,6 +33,9 @@ public class NavigationFragment extends Fragment {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getChildFragmentManager();
+            while (fragmentManager.getBackStackEntryCount() != 0)
+                fragmentManager.popBackStackImmediate();
             Class<? extends Fragment> fragment = fragmentMap.get(item.getItemId());
             try {
                 Fragment instance = fragment.newInstance();
