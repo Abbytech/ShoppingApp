f750679499df600decfad99c00a6036c2a82e685
Rado <bsawsk@hotmail.com>
2017-04-02 19:44:00 +0300

    added aisle argument.

--- app/src/main/java/com/abbytech/shoppingapp/shop/aisles/AislesController.java
+++ app/src/main/java/com/abbytech/shoppingapp/shop/aisles/AislesController.java
@@ -1,11 +1,13 @@
 package com.abbytech.shoppingapp.shop.aisles;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 
 import com.abbytech.shoppingapp.framework.ActionController;
 import com.abbytech.shoppingapp.shop.OnAisleActionListener;
+import com.abbytech.shoppingapp.shop.ShopFragment;
 import com.abbytech.shoppingapp.shoppinglist.ShopActionFragment;
 
 public class AislesController extends ActionController<Aisle> implements OnAisleActionListener {
@@ -16,7 +18,10 @@ public class AislesController extends ActionController<Aisle> implements OnAisle
     @Override
     public void onItemAction(Aisle item, @Action int action) {
         int id = getFragment().getId();
+        Bundle bundle = new Bundle();
+        bundle.putString(ShopFragment.EXTRA_AISLE, item.name);
         ShopActionFragment shopFragment = new ShopActionFragment();
+        shopFragment.setArguments(bundle);
         getFragment()
                 .getFragmentManager()
                 .beginTransaction()
