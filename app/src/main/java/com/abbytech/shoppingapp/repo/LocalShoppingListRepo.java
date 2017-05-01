16a8514ab0b0e6a91ca8de653e19826ec941d526
Rado <bsawsk@hotmail.com>
2017-05-01 17:08:16 +0300

    removed pointless ObservableArrayList; changes are relayed via controllers instead. fixed bug where loading a shopping list results in stale items.

--- app/src/main/java/com/abbytech/shoppingapp/repo/LocalShoppingListRepo.java
+++ app/src/main/java/com/abbytech/shoppingapp/repo/LocalShoppingListRepo.java
@@ -1,9 +1,6 @@
 package com.abbytech.shoppingapp.repo;
 
 
-import android.databinding.ObservableArrayList;
-import android.databinding.ObservableList;
-
 import com.abbytech.shoppingapp.model.DaoSession;
 import com.abbytech.shoppingapp.model.Item;
 import com.abbytech.shoppingapp.model.ListItem;
@@ -30,36 +27,6 @@ public class LocalShoppingListRepo implements IShoppingListRepo {
     @Override
     public Observable<ShoppingList> getShoppingList(int id) {
         ShoppingList shoppingList = loadShoppingList(id);
-        ObservableArrayList<ListItem> items = new ObservableArrayList<>();
-        items.addAll(shoppingList.getItems());
-        shoppingList.setItems(items);
-        items.addOnListChangedCallback(new ObservableList.OnListChangedCallback<ObservableList<ListItem>>() {
-            @Override
-            public void onChanged(ObservableList<ListItem> listItems) {
-                shoppingList.update();
-            }
-
-            @Override
-            public void onItemRangeChanged(ObservableList<ListItem> listItems, int i, int i1) {
-                for (int j=i;j<i+i1;j++)
-                listItems.get(j).update();
-            }
-
-            @Override
-            public void onItemRangeInserted(ObservableList<ListItem> listItems, int i, int i1) {
-                shoppingList.update();
-            }
-
-            @Override
-            public void onItemRangeMoved(ObservableList<ListItem> listItems, int i, int i1, int i2) {
-                shoppingList.update();
-            }
-
-            @Override
-            public void onItemRangeRemoved(ObservableList<ListItem> listItems, int i, int i1) {
-                shoppingList.update();
-            }
-        });
         return Observable.just(shoppingList);
     }
 
@@ -81,6 +48,7 @@ public class LocalShoppingListRepo implements IShoppingListRepo {
             daoSession.getItemDao().insertOrReplaceInTx(items);
         }
         shoppingListDao.insertOrReplace(list);
+        list.resetItems();
     }
     @Override
     public Observable<List<ShoppingList>> getShoppingLists() {
